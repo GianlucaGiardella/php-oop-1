12923eb296f49902a17cab287c366f25b46255d8
GianluGiarde <gianlugiarde@gmail.com>
2023-06-19 20:00:06 +0200

    Add: genres foreach / Change: titles into headings

--- index.php
+++ index.php
@@ -17,21 +17,27 @@ include __DIR__ . "/data.php";
 
 <body>
     <div class="container">
+        <?php
+        foreach ($movies as $movie) { ?>
+        <h2>Movies:</h2>
         <ul>
-            <li><strong>Movies:</strong></li>
-            <?php
-            foreach ($movies as $movie) { ?>
             <li><?= $movie->title  ?></li>
             <li><?= $movie->year ?></li>
             <li><?= $movie->language ?></li>
             <li><?= $movie->duration ?></li>
             <li>
+                <h3>Actors:</h3>
                 <ul>
-                    <li>
-                        <strong>Actors:</strong>
-                    </li>
                     <?php foreach ($movie->actors as $actor) { ?>
                     <li><?= $actor->get_actor_role() ?></li>
+                    <?php } ?>
+                </ul>
+            </li>
+            <li>
+                <h3>Genres:</h3>
+                <ul>
+                    <?php foreach ($movie->genres as $genre) { ?>
+                    <li><?= $genre->name ?></li>
                     <?php } ?>
                 </ul>
             </li>
