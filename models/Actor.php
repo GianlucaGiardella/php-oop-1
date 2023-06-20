8db291fb7080647c94286790c721d2d3b419dc63
GianluGiarde <gianlugiarde@gmail.com>
2023-06-20 16:11:34 +0200

    Fix: attributes

--- models/Actor.php
+++ models/Actor.php
@@ -2,21 +2,15 @@
 
 class Actor
 {
-    public int $id;
-    public string $name;
-    public string $movie_name;
-    public int $gender;
-    public bool $adult;
     public static int $actor_counter = 0;
 
-    public function __construct(int $id, string $name, string $movie_name, int $gender, bool $adult)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->movie_name = $movie_name;
-        $this->gender = $gender;
-        $this->adult = $adult;
-
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $movie_name,
+        public int $gender,
+        public bool $adult
+    ) {
         self::$actor_counter += 1;
     }
 
--- models/Movie.php
+++ models/Movie.php
@@ -2,36 +2,29 @@
 
 class Movie
 {
-    public int $id;
-    public string $title;
-    public int $duration;
-    public int $year;
-    public string $language;
-    public array $actors;
-    public array $genres;
     public static int $counter = 0;
 
-    public function __construct(int $id, string $title, int $duration, int $year, string $language, array $actors = [], array $genres = [])
-    {
-        $this->id = $id;
-        $this->title = $title;
-        $this->duration = $duration;
-        $this->year = $year;
-        $this->language = $language;
+    public function __construct(
+        public int $id,
+        public string $title,
+        public int $duration,
+        public int $year,
+        public string $language,
+        public array $actors,
+        public array $genres
+    ) {
 
         foreach ($actors as $actor) {
             if (!$actor instanceof Actor) {
                 die("L'oggetto deve essere del tipo di Actor");
             }
         };
-        $this->actors = $actors;
 
         foreach ($genres as $genre) {
             if (!$genre instanceof Genre) {
                 die("L'oggetto deve essere del tipo di Genre");
             }
         };
-        $this->genres = $genres;
 
         self::$counter += 1;
     }
